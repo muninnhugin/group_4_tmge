efd91f8be314732be793ef4f267b86f3f1027bcb
cindyp <cindya1004@gmail.com>
2023-02-23 18:01:00 -0800

    create different main files for each game

--- TileMatchingGameEngine/src/App.java
+++ TileMatchingGameEngine/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Choose what game to play: 1. tictactoe 2.bejeweled 3.game");
     }
 }
