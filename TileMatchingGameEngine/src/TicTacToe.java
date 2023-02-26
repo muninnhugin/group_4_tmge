e478cb0252fed0831a1c2a6dec91c667ad5d4246
TheChilledLemon <40331455+TheChilledLemon@users.noreply.github.com>
2023-02-26 13:55:18 -0800

    initial implentation of tictactoe loop

--- TileMatchingGameEngine/src/TicTacToe.java
+++ TileMatchingGameEngine/src/TicTacToe.java
@@ -1,3 +1,31 @@
 public class TicTacToe {
+    TileMatrix matrix;
+    TileSpawnLogic spawn;
+    TileMatchingLogic match;
+    TileDestructionLogic destruct;
+    Player player1;
+    Player player2;
     
+    public void run {
+        // Load player data.
+        player1 = new Player();
+        player2 = new Player();
+        matrix = new TileMatrix(3, 3);
+        spawn = new PlaceLogic(matrix); // Placeholder name.
+        match = new TicTacToeMatch(matrix) // Placeholder name.
+        spawn.init();
+        String move;
+        boolean win;
+        while (true) {
+            move = player1.getInput(); // Could replace w/ move object?
+            win = match.makeMove(move); // How can match determine who wins? Are they responsible for it or someone else.
+            if (win) break;
+            spawn.makeMove(move);
+            
+            String move = player1.getInput();
+            boolean end = match.makeMove(move);
+            if (end) break;
+            spawn.makeMove(move);
+        }
+    }
 }
