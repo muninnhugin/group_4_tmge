7146f269e7a5fdc60a4465daffc87b81cfa6b8f0
muninnhugin <muninnhugin@gmail.com>
2023-03-19 14:32:58 -0700

    add equals() and hashCode() to Coordinate and equals() to Tile

--- TileMatchingGameEngine/src/tmge/Coordinate.java
+++ TileMatchingGameEngine/src/tmge/Coordinate.java
@@ -1,17 +1,42 @@
 package tmge;
 
-public class Coordinate implements Comparable<Coordinate>{
+public class Coordinate{
     public int x;
     public int y;
     public Coordinate(int x, int y){
         this.x = x;
         this.y = y;
     }
+
+    public Coordinate(Coordinate coordinate)
+    {
+        x = coordinate.x;
+        y = coordinate.y;
+    }
+
     @Override
-    public int compareTo(Coordinate o) {
-        // TODO Auto-generated method stub
-        if(this.x == o.x && this.y == o.y)  return 0;
-        // if(this.x < )
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Coordinate other = (Coordinate) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
     }
     
 }
