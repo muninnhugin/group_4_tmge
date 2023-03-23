e0be15f861ca6907681e3bfea0c71a294acfdaef
Christine <71248843+christinetn@users.noreply.github.com>
2023-03-23 01:30:55 -0700

    add gui

--- TileMatchingGameEngine/src/tmgeclient/hellofx.java
+++ TileMatchingGameEngine/src/tmgeclient/hellofx.java
@@ -4,8 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.stage.Stage;
+import tmge.Coordinate;
+import tmge.Tile;
 import tmge.TileMatrix;
+import tmge.logic.FullSpawn;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.GridPane;
@@ -22,51 +28,81 @@ public class hellofx extends Application {
 
     Stage ps;
     GridPane pane;
+    TileMatrix tm = new TileMatrix(8,8);
+
+    int tileSize = 80;
+
+
+    
+
+    public void startGame(TileMatrix matrix, String[] args)
+    {
+        tm = matrix;
+        launch(args);
+
+    }
+
+ 
+    private Image createImage(Color color) {
+        WritableImage image = new WritableImage(1, 1);
+        image.getPixelWriter().setColor(0, 0, color);
+        return image ;
+    }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.ps = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        this.ps.setScene(new Scene(initGrid(root)));
-        this.ps.show();
-    }
 
-    private Parent initGrid(Parent root)
-    {
-
-        int rows = 5;
-        int columns = 5;
         this.ps.setTitle("TMGE");
         this.ps.setResizable(false);
-        pane = new GridPane();
+        //primaryStage.setScene(new Scene(root, 700, 500));
 
-         
-        for(int i = 0; i < columns; i++) {
-            pane.getColumnConstraints().add(new ColumnConstraints(100));
+        GridPane gridPane = new GridPane();
+
+        // for visualizing the different squares:
+        gridPane.setHgap(2);
+        gridPane.setVgap(2);
+        gridPane.setStyle("-fx-background-color: black;");
+        //this.ps.setScene(new Scene(initGrid(root)));
+        FullSpawn fs = new FullSpawn();
+        fs.spawn(tm);
+
+        for (int y = 0 ; y < tm.getRow() ; y++) {
+            for (int x = 0 ; x < tm.getColumn() ; x++) {
+                Image tc = createImage(Color.BLACK);
+                if (tm.getTile(new Coordinate(x,y)).getColor() == "B") {
+                    tc = createImage(Color.BLUE);
+                } else if (tm.getTile(new Coordinate(x,y)).getColor() == "Y") {
+                    tc = createImage(Color.YELLOW);
+                } else if (tm.getTile(new Coordinate(x,y)).getColor() == "P") {
+                    tc = createImage(Color.PURPLE);
+                } else if (tm.getTile(new Coordinate(x,y)).getColor() == "R") {
+                    tc = createImage(Color.RED);
+                } else if (tm.getTile(new Coordinate(x,y)).getColor() == "G") {
+                    tc = createImage(Color.GREEN);
+                }
+                ImageView imageView = new ImageView(tc);
+                imageView.setFitWidth(tileSize);
+                imageView.setFitHeight(tileSize);
+                gridPane.add(imageView, x, y);
+            }
         }
-
-        for(int i = 0; i < rows; i++) {
-            pane.getRowConstraints().add(new RowConstraints(100));
-        }
-
-        pane.setGridLinesVisible(true);
-        //Shape sh = makeGrid();
-        //grid.getChildren().add(sh);
-        return pane;
+        Scene scene = new Scene(gridPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+      
     }
 
+  
 
 
     /* 
-    public Shape makeGrid()
-    {
-        Shape shape = new Circle();
-        return shape;
-    }
-    */
-
     public static void main(String[] args) {
         launch(args);
     }
+
+    */
 }
