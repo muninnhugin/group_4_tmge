e7504d52403aa52d5685cdf7ab2baf59081284f0
cindyp <cindya1004@gmail.com>
2023-03-23 19:19:43 -0700

    bejeweled directory made & turnlimit logic

---
+++ TileMatchingGameEngine/src/bejeweled/Bejeweled.java
@@ -0,0 +1,22 @@
+package bejeweled;
+import java.util.ArrayList;
+import java.util.List;
+
+import tmge.TMGE;
+import tmge.logic.*;
+
+public class Bejeweled {
+    public static void main(String[] args) {
+        TMGE tmge = new TMGE(5, 5, 1);
+        tmge.setSpawnLogic(new EmptySpawn());
+        tmge.setMatchingLogic(List.of(new HorizontalMatching(3), new VerticalMatching(3)));
+        tmge.setDestructionLogic(new GravityDestructionLogic(true));
+        tmge.setEndLogic(new TurnLimitEndLogic());
+        tmge.setMoveLogic(new T3MoveLogic());
+        try {
+            tmge.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
